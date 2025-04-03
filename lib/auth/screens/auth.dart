203f073f69bb410222fa07efbddc354feb40cf56
Abhiroop Santra <abhiroop.santra@gmail.com>
2025-04-03 14:04:46 +0400

    feat: enhance registration form with validation and navigation to dashboard

--- lib/auth/screens/auth.dart
+++ lib/auth/screens/auth.dart
@@ -1,5 +1,6 @@
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:flutter_expense_tracker/auth/widgets/text_box_with_icon.dart';
+import 'package:flutter_expense_tracker/expenses/screens/dashboard.dart';
 
 class AuthScreen extends StatefulWidget {
   final bool isLogin;
@@ -13,10 +14,92 @@ class AuthScreen extends StatefulWidget {
 class _AuthScreenState extends State<AuthScreen> {
   final _formKey = GlobalKey<FormState>();
 
-  final nameController = TextEditingController();
-  final emailController = TextEditingController();
-  final passwordController = TextEditingController();
-  final confirmPasswordController = TextEditingController();
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _confirmPasswordController = TextEditingController();
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    _confirmPasswordController.dispose();
+    super.dispose();
+  }
+
+  void _submitForm() async {
+    try {
+      if (_formKey.currentState == null || !_formKey.currentState!.validate()) {
+        debugPrint("Form is not valid");
+        return;
+      }
+
+      // if (_passwordController.text != _confirmPasswordController.text) {
+      //   await displayInfoBar(
+      //     context,
+      //     builder: (context, close) {
+      //       return InfoBar(
+      //         title: const Text('Error: Passwords do not match'),
+      //         action: IconButton(
+      //           icon: const Icon(FluentIcons.clear),
+      //           onPressed: close,
+      //         ),
+      //         severity: InfoBarSeverity.error,
+      //       );
+      //     },
+      //   );
+      //   return;
+      // }
+
+      _formKey.currentState!.save();
+
+      await displayInfoBar(
+        context,
+        builder: (context, close) {
+          return InfoBar(
+            title: const Text('Success'),
+            content: const Text('Successfully registered.'),
+            action: IconButton(
+              icon: const Icon(FluentIcons.clear),
+              onPressed: close,
+            ),
+            severity: InfoBarSeverity.success,
+          );
+        },
+      );
+
+      if (!mounted) {
+        return;
+      }
+
+      Navigator.of(
+        context,
+      ).push(FluentPageRoute(builder: (context) => DashboardScreen()));
+    } catch (error) {
+      debugPrint(error.toString());
+
+      if (!mounted) {
+        return;
+      }
+
+      await displayInfoBar(
+        context,
+        builder: (context, close) {
+          return InfoBar(
+            title: const Text(
+              'Unable to register user. Please try again later.',
+            ),
+            action: IconButton(
+              icon: const Icon(FluentIcons.clear),
+              onPressed: close,
+            ),
+            severity: InfoBarSeverity.error,
+          );
+        },
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -45,78 +128,125 @@ class _AuthScreenState extends State<AuthScreen> {
           ],
         ),
       ),
-      content: Container(
-        margin: EdgeInsets.fromLTRB(20, 50, 20, 20),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    "Let's\nGet Started",
-                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.w900),
-                  ),
-                  SizedBox(width: 10),
-                ],
-              ),
-              SizedBox(height: 30),
-              Text("Create an account to track your expenses"),
-              SizedBox(height: 10),
-              TextBoxWithIcon(
-                textEditingController: nameController,
-                placeholder: "Enter your name",
-                icon: FluentIcons.people_add,
-                keyboardType: TextInputType.name,
-              ),
-              TextBoxWithIcon(
-                textEditingController: nameController,
-                placeholder: "Enter your email",
-                icon: FluentIcons.mail,
-                keyboardType: TextInputType.name,
-              ),
-              TextBoxWithIcon(
-                textEditingController: nameController,
-                placeholder: "Enter a password",
-                icon: FluentIcons.password_field,
-                keyboardType: TextInputType.name,
-              ),
-              TextBoxWithIcon(
-                textEditingController: nameController,
-                placeholder: "Re-enter the password",
-                icon: FluentIcons.password_field,
-                keyboardType: TextInputType.name,
-              ),
-              SizedBox(height: 15),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  FilledButton(
-                    onPressed: () {},
-                    style: ButtonStyle(
-                      padding: WidgetStateProperty.all(
-                        EdgeInsets.symmetric(vertical: 12, horizontal: 0),
-                      ),
-                      shape: WidgetStateProperty.all(
-                        RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(18.0),
-                        ),
-                      ),
-                    ),
-                    child: Text(
-                      "Sign Up",
+      content: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.fromLTRB(20, 50, 20, 20),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Let's\nGet Started",
                       style: TextStyle(
-                        fontSize: 18,
+                        fontSize: 30,
                         fontWeight: FontWeight.w900,
                       ),
                     ),
-                  ),
-                ],
-              ),
-            ],
+                    SizedBox(width: 10),
+                  ],
+                ),
+                SizedBox(height: 30),
+                Text("Create an account to track your expenses"),
+                SizedBox(height: 10),
+                TextBoxWithIcon(
+                  formKey: _formKey,
+                  textEditingController: _nameController,
+                  placeholder: "Enter your name",
+                  icon: FluentIcons.people_add,
+                  keyboardType: TextInputType.name,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your name.';
+                    }
+                    if (value.length < 3 || value.length > 50) {
+                      return 'Name must be between 3 and 50 characters.';
+                    }
+                    return null;
+                  },
+                ),
+                TextBoxWithIcon(
+                  formKey: _formKey,
+                  textEditingController: _emailController,
+                  placeholder: "Enter your email",
+                  icon: FluentIcons.mail,
+                  keyboardType: TextInputType.emailAddress,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your email.';
+                    }
+                    return null;
+                  },
+                ),
+                TextBoxWithIcon(
+                  formKey: _formKey,
+                  textEditingController: _passwordController,
+                  placeholder: "Enter a password",
+                  icon: FluentIcons.password_field,
+                  keyboardType: TextInputType.text,
+                  isPassword: true,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter a password.';
+                    }
+                    if (value.length < 6 || value.length > 50) {
+                      return 'Password must be between 6 and 50 characters';
+                    }
+                    return null;
+                  },
+                ),
+                TextBoxWithIcon(
+                  formKey: _formKey,
+                  textEditingController: _confirmPasswordController,
+                  placeholder: "Re-enter the password",
+                  icon: FluentIcons.password_field,
+                  keyboardType: TextInputType.text,
+                  isPassword: true,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please re-enter the password.';
+                    }
+                    if (value.length < 6 || value.length > 50) {
+                      return 'Password must be between 6 and 50 characters';
+                    }
+                    if (value != _passwordController.text) {
+                      return 'Passwords do not match.';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 15),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    FilledButton(
+                      onPressed: _submitForm,
+                      style: ButtonStyle(
+                        padding: WidgetStateProperty.all(
+                          EdgeInsets.symmetric(vertical: 12, horizontal: 0),
+                        ),
+                        shape: WidgetStateProperty.all(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(18.0),
+                          ),
+                        ),
+                      ),
+                      child: Text(
+                        "Sign Up",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w900,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/auth/widgets/text_box_with_icon.dart
+++ lib/auth/widgets/text_box_with_icon.dart
@@ -1,10 +1,13 @@
 import 'package:fluent_ui/fluent_ui.dart';
 
-class TextBoxWithIcon extends StatelessWidget {
+class TextBoxWithIcon extends StatefulWidget {
   final TextEditingController textEditingController;
   final String placeholder;
   final IconData icon;
   final TextInputType keyboardType;
+  final FormFieldValidator<String>? validator;
+  final bool isPassword;
+  final GlobalKey<FormState> formKey;
 
   const TextBoxWithIcon({
     super.key,
@@ -12,34 +15,65 @@ class TextBoxWithIcon extends StatelessWidget {
     required this.placeholder,
     required this.icon,
     required this.keyboardType,
+    this.validator,
+    this.isPassword = false,
+    required this.formKey,
   });
+
+  @override
+  State<TextBoxWithIcon> createState() => _TextBoxWithIconState();
+}
+
+class _TextBoxWithIconState extends State<TextBoxWithIcon> {
+  bool hasError = false;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
-      child: TextBox(
-        keyboardType: keyboardType,
-        foregroundDecoration: WidgetStateProperty.all(
-          BoxDecoration(
-            border: Border.all(color: Colors.white),
-            borderRadius: BorderRadius.circular(15),
+      child: Focus(
+        onFocusChange: (focused) {
+          if (!focused && widget.validator != null) {
+            final error = widget.validator!(widget.textEditingController.text);
+            setState(() {
+              hasError = error != null;
+            });
+          }
+        },
+        child: TextFormBox(
+          keyboardType: widget.keyboardType,
+          highlightColor: Colors.transparent,
+          unfocusedColor: Colors.transparent,
+          errorHighlightColor: Colors.red,
+          // foregroundDecoration: WidgetStateProperty.all(
+          //   BoxDecoration(
+          //     border: Border.all(color: Colors.white),
+          //     borderRadius: BorderRadius.circular(15),
+          //   ),
+          // ),
+          obscureText: widget.isPassword,
+          obscuringCharacter: '*',
+          autocorrect: !widget.isPassword,
+          maxLength: 50,
+          controller: widget.textEditingController,
+          placeholder: widget.placeholder,
+          expands: false,
+          style: TextStyle(height: 2.5),
+          padding: EdgeInsets.fromLTRB(12, 8, 12, 12),
+          prefix: Padding(
+            padding: EdgeInsets.only(left: 8.0, right: 4.0),
+            child: Icon(
+              widget.icon,
+              size: 25,
+            ), // Replace with your desired icon
           ),
-        ),
-        controller: textEditingController,
-        placeholder: placeholder,
-        expands: false,
-        style: TextStyle(height: 2.5),
-        padding: EdgeInsets.fromLTRB(12, 8, 12, 12),
-        prefix: Padding(
-          padding: EdgeInsets.only(left: 8.0, right: 4.0),
-          child: Icon(icon, size: 25), // Replace with your desired icon
-        ),
-        decoration: WidgetStateProperty.all(
-          BoxDecoration(
-            border: Border.all(color: Colors.white),
-            borderRadius: BorderRadius.circular(15),
-          ), // Optional: for rounded corners
+          decoration: WidgetStateProperty.all(
+            BoxDecoration(
+              border: Border.all(color: hasError ? Colors.red : Colors.white),
+              borderRadius: BorderRadius.circular(15),
+            ),
+          ),
+          validator: widget.validator,
         ),
       ),
     );
