2fc9856ed36fe8f69ece5f0b00c7c6ba9ab9fe81
Abhiroop Santra <abhiroop43@users.noreply.github.com>
2025-05-04 02:41:14 +0400

    feat: enhance profiles screen with user information and layout

--- lib/expenses/screens/profiles.dart
+++ lib/expenses/screens/profiles.dart
@@ -10,6 +10,58 @@ class ProfilesScreen extends StatefulWidget {
 class _ProfilesScreenState extends State<ProfilesScreen> {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("This is the profiles screen"));
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Column(
+          children: [
+            Container(
+              margin: const EdgeInsets.only(top: 25),
+              child: Text(
+                'Profile',
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 25),
+              child: CircleAvatar(
+                backgroundColor: Colors.blue,
+                backgroundImage: NetworkImage(
+                  "https://play-lh.googleusercontent.com/hJGHtbYSQ0nCnoEsK6AGojonjELeAh_Huxg361mVrPmzdwm8Ots-JzEH5488IS2nojI",
+                ),
+                // AssetImage('assets/images/avatar1.jpeg'),
+                radius: 100,
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 10),
+              child: Text(
+                'Abhiroop Santra',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.w800,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 2),
+              child: Text(
+                'abhiroop.santra@gmail.com',
+                style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w300,
+                  color: Color(0xE7E7E7FF),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
   }
 }
