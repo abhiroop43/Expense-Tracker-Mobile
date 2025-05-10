3b94cd5771dfee48a1449144f4911c577b936919
Abhiroop Santra <abhiroop43@users.noreply.github.com>
2025-05-10 09:23:56 +0400

    fix: integrate flutter_screenutil for responsive design in main app layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:flutter_expense_tracker/common/colors.dart';
 import 'package:flutter_expense_tracker/expenses/screens/home.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 // import 'package:flutter_expense_tracker/start.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -14,21 +15,26 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return FluentApp(
-      title: 'Expense Tracker',
-      debugShowCheckedModeBanner: false,
-      themeMode: ThemeMode.dark,
-      darkTheme: FluentThemeData(
-        fontFamily: GoogleFonts.poppins().fontFamily ?? 'DefaultFont',
-        brightness: Brightness.dark,
-        accentColor: AccentColor.swatch({'normal': ThemeColors.primaryColor}),
-        scaffoldBackgroundColor: ThemeColors.backgroundColor,
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: false,
+      child: FluentApp(
+        title: 'Expense Tracker',
+        debugShowCheckedModeBanner: false,
+        themeMode: ThemeMode.dark,
+        darkTheme: FluentThemeData(
+          fontFamily: GoogleFonts.poppins().fontFamily ?? 'DefaultFont',
+          brightness: Brightness.dark,
+          accentColor: AccentColor.swatch({'normal': ThemeColors.primaryColor}),
+          scaffoldBackgroundColor: ThemeColors.backgroundColor,
+        ),
+        theme: FluentThemeData(
+          fontFamily: GoogleFonts.poppins().fontFamily ?? 'DefaultFont',
+          accentColor: AccentColor.swatch({'normal': ThemeColors.primaryColor}),
+        ),
+        home: HomeScreen(), // StartScreen(),
       ),
-      theme: FluentThemeData(
-        fontFamily: GoogleFonts.poppins().fontFamily ?? 'DefaultFont',
-        accentColor: AccentColor.swatch({'normal': ThemeColors.primaryColor}),
-      ),
-      home: HomeScreen(), // StartScreen(),
     );
   }
 }
