29daf05881daba2baf557ce6798a0d73eb5ed9cb
Abhiroop Santra <abhiroop.santra@gmail.com>
2025-06-10 14:02:47 +0400

    feat(image_input): :sparkles: enhance ImageInput widget to support receipt selection
    * Updated Info.plist to reflect new permissions for accessing photo library for receipt uploads.
    * Implemented image selection functionality in ImageInput widget to allow users to select receipt images from the gallery.

--- lib/expenses/widgets/image_input.dart
+++ lib/expenses/widgets/image_input.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:flutter_expense_tracker/common/colors.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ImageInput extends StatefulWidget {
   const ImageInput({super.key});
@@ -9,10 +12,57 @@ class ImageInput extends StatefulWidget {
 }
 
 class _ImageInputState extends State<ImageInput> {
+  File? _selectedFile;
+
+  void _selectReceiptImage() async {
+    final imagePicker = ImagePicker();
+    final pickedFile = await imagePicker.pickImage(
+      source: ImageSource.gallery,
+      maxWidth: 600,
+      maxHeight: 600,
+      imageQuality: 80,
+    );
+
+    if (pickedFile == null) {
+      return;
+    }
+
+    setState(() {
+      _selectedFile = File(pickedFile.path);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    var screenHeight = MediaQuery.of(context).size.height;
+    double screenHeight = MediaQuery.of(context).size.height;
     double screenWidth = MediaQuery.of(context).size.width;
+
+    Widget? content = Button(
+      onPressed: _selectReceiptImage,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(FluentIcons.camera, size: screenHeight * 0.02),
+          SizedBox(width: screenHeight * 0.02),
+          Text(
+            'Select Receipt',
+            style: TextStyle(fontSize: screenHeight * 0.015),
+          ),
+        ],
+      ),
+    );
+
+    if (_selectedFile != null) {
+      content = GestureDetector(
+        onTap: _selectReceiptImage,
+        child: Image.file(
+          _selectedFile!,
+          fit: BoxFit.cover,
+          width: double.infinity,
+          height: double.infinity,
+        ),
+      );
+    }
 
     return Container(
       margin: const EdgeInsets.all(10),
@@ -26,27 +76,7 @@ class _ImageInputState extends State<ImageInput> {
       width: double.infinity,
       alignment: Alignment.center,
 
-      // child: IconButton(
-      //   icon: Icon(FluentIcons.camera, size: screenHeight * 0.04),
-      //   onPressed: () => debugPrint('pressed button'),
-      // ),
-      child: SizedBox(
-        width: screenWidth * 0.5,
-        child: Button(
-          onPressed: () {},
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(FluentIcons.camera, size: screenHeight * 0.02),
-              SizedBox(width: screenHeight * 0.02),
-              Text(
-                'Select Receipt',
-                style: TextStyle(fontSize: screenHeight * 0.015),
-              ),
-            ],
-          ),
-        ),
-      ),
+      child: SizedBox(width: screenWidth * 0.5, child: content),
     );
   }
 }
