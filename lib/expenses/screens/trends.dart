69384760a4489aa3717177119b9f3c2c19f6bc2c
Abhiroop Santra <abhiroop.santra@gmail.com>
2025-06-13 16:04:55 +0400

    feat(profiles): :sparkles: increase margin for profile title
    * Adjusted the top margin of the profile title for better visual spacing.

--- lib/expenses/screens/trends.dart
+++ lib/expenses/screens/trends.dart
@@ -1,4 +1,5 @@
 import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter_expense_tracker/expenses/widgets/transaction_chart.dart';
 
 class TrendsScreen extends StatefulWidget {
   const TrendsScreen({super.key});
@@ -10,6 +11,26 @@ class TrendsScreen extends StatefulWidget {
 class _TrendsScreenState extends State<TrendsScreen> {
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView();
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.only(top: 35),
+            child: Text(
+              'Statistics',
+              style: TextStyle(
+                fontSize: MediaQuery.of(context).size.width * 0.06,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          TransactionChart(),
+        ],
+      ),
+    );
   }
 }
