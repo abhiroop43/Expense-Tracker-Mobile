1cd68c5460d4e922e6064d7564db40f3ab941454
Abhiroop Santra <abhiroop43@users.noreply.github.com>
2025-06-23 00:56:01 +0400

    feat(auth): :sparkles: implement password reset form with validation
    * Added a stateful widget for the password reset screen.
    * Included form validation for email input.
    * Enhanced user experience with a responsive layout and button.

--- lib/auth/screens/password_reset.dart
+++ lib/auth/screens/password_reset.dart
@@ -1,12 +1,75 @@
 import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter_expense_tracker/auth/widgets/text_box_with_icon.dart';
+import 'package:flutter_expense_tracker/common/extensions.dart';
 
-class PasswordResetScreen extends StatelessWidget {
+class PasswordResetScreen extends StatefulWidget {
   const PasswordResetScreen({super.key});
 
   @override
+  State<PasswordResetScreen> createState() => _PasswordResetScreenState();
+}
+
+class _PasswordResetScreenState extends State<PasswordResetScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    super.dispose();
+  }
+
+  void _submitForm() {}
+
+  @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+
     return ScaffoldPage(
-      content: Center(child: Text("This is the password reset screen.")),
+      content: SingleChildScrollView(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              TextBoxWithIcon(
+                formKey: _formKey,
+                textEditingController: _emailController,
+                placeholder: 'john.doe@example.com',
+                icon: FluentIcons.accounts,
+                keyboardType: TextInputType.emailAddress,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'An email id is required';
+                  }
+
+                  if (!value.isValidEmail()) {
+                    return 'A valid email id is required';
+                  }
+                  return null;
+                },
+              ),
+
+              SizedBox(height: 15),
+
+              FilledButton(
+                onPressed: _submitForm,
+                style: ButtonStyle(
+                  padding: WidgetStateProperty.all(
+                    EdgeInsets.symmetric(
+                      vertical: 12,
+                      horizontal: screenWidth * 0.3,
+                    ),
+                  ),
+                ),
+                child: Text(
+                  "Reset Password",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w900),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/expenses/widgets/search.dart
+++ lib/expenses/widgets/search.dart
@@ -20,6 +20,12 @@ class _SearchModalState extends State<SearchModal> {
   }
 
   @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     double screenHeight = MediaQuery.of(context).size.height;
     double screenWidth = MediaQuery.of(context).size.width;
